f54f4ed66ed250c50b41beee04b6f3694cea0562
MinLinSTAT <your.email@example.com>
2024-09-04 11:35:36 -0400

    Easter Egg

--- R/YD.R
+++ R/YD.R
@@ -1,8 +1,7 @@
 #' A sweet hidden message
 #'
 #' @description This function is a hidden message for someone special.
-#' @export
-
-.sweet_message <- function() {
+#' @keywords internal
+sweet_message <- function() {
   message("Today is gonna be an amazing day! And I love you.")
 }
